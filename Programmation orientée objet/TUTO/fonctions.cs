99a31e82be4b2432c41b00fbbffdbd8da4ddac13
YLIssakha234 <yayalibis05@gmail.com>
2024-10-14 16:59:16 +0200

    first commit

---
+++ Programmation orientée objet/TUTO/fonctions.cs
@@ -0,0 +1,27 @@
+
+
+//une fonction est composée de 3 elts
+
+void maFonction(int monlnt)
+
+// void = type
+// maFonction = nom
+// int monlnt = paramètres optionnel
+
+void Start()
+
+// est une fnc qui se lance lors du demarage
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class test: MonoBehaviour{
+    int monInt =5;
+
+    void Start(){
+        print(monInt);
+        
+    }
+}
