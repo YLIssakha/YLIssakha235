0ae201bd42a4b292071c865b2c130cc984da311e
YLIssakha234 <yayalibis05@gmail.com>
2024-10-17 22:44:00 +0200

    first commit

--- Programmation orientée objet/TUTO/ConsoleApp1/Program.cs
+++ Programmation orientée objet/TUTO/ConsoleApp1/Program.cs
@@ -1,39 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// 
 
-// c'est un commenctaire qui permet de donner des indications
+// creation de classe 
 
-// using System.Security.Cryptography.X509Certificates;
-
-
-
-using System.Diagnostics.Contracts;
-
-Console.WriteLine("Hello, World!");
-
-
-// console = objetà utiliser
-// Wriline = ecrire une ligne est une methode d'objet
-// ()= passage de paramètres
-
-Console.WriteLine("j'apprends le C#");
-
-// on fait pas attention à la surligne jaune car c'est un avertissement de compilateur
-
-// var NOM = Valeur on utilise var pour pas avoir des int/...
-// c'est une facilité d'écriture 
-var taille = 182;
-var nom = "Mommer";
-var faux = false;
-
-// il est preferable d'utiliser le type a lieu de var
-
-// une constante est une donnée qu'on ne peut pas changer 
-
-const int valeur = 32;
-const string pays = "France";
-const bool vrai = true;
-
-// affectation de variable
-
-var sasie = ConsoleModifiers.ReadLine();
-
+class Contact 
+ {
+    public void setphonenumber(int phonenumber){
+        this.phonenumber = phonenumber;
+    }
+ }
--- Programmation orientée objet/TUTO/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Programmation orientée objet/TUTO/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a31e82be4b2432c41b00fbbffdbd8da4ddac13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12446ce6266e81af61bcf2d6b88ff2646f81dc84")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
